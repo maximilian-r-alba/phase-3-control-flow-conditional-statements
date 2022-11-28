a05b6a69b56e1e82e799fc5110a37a4424ee449f
Maximilian Rex Alba <albam476@gmail.com>
2022-11-27 21:18:45 -0500

    commit tree 477aa2b9dbfcc9fd2aa0bb171f6d8871f2f368ea

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,47 @@
+require 'pry'
 def admin_login(username, password)
-  # your code here
+  if username.downcase == "admin" && password == '12345' || username.upcase == "ADMIN" && password == '12345'
+     "Access granted"
+  else
+     "Access denied"
+  end
+  
 end
 
 def hows_the_weather(temperature)
-  # your code here
+  
+  case
+  when (temperature < 40) then "It's brisk out there!"
+  when (temperature > 40 && temperature < 65)
+    "It's a little chilly out there!"
+  when temperature > 85
+    "It's too dang hot out there!"
+  else
+    "It's perfect out there!"
+  end
+  
 end
 
 def fizzbuzz(num)
   # your code here
+  case 
+  when (num % 3 ==  0 && num % 5 == 0) then 'FizzBuzz'
+  when num % 3 == 0 then 'Fizz'
+  when num % 5 == 0 then 'Buzz'
+  else num
+  end
+
 end
 
 def calculator(operation, num1, num2)
   # your code here
+  case operation
+  when "+" then num1 + num2
+  when "-" then num1 - num2
+  when "*" then num1 * num2
+  when "/" then num1 / num2
+  else puts "Invalid operation!"
+  end
+
 end
 
